0272b7de2fa0261f99d3381cb271cdfac21e1d64
Hồ Hoàng Việt Tiến <50546161+hotien2107@users.noreply.github.com>
2021-01-13 20:25:42 +0700

    add login and register

--- SaiGonFood/controllers/User.Controller.js
+++ SaiGonFood/controllers/User.Controller.js
@@ -1,7 +1,36 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("model/db.json");
+
+db = low(adapter);
+
+// Set some defaults
+db.defaults({ user: [] }).write();
+
 module.exports.login = (req, res, next) => {
-    res.render('auth/login');
-}
+  const account = req.body.account;
+  const password = req.body.pass;
+
+  const user = db.get('user').find({account: account}).value();
+
+  if(!user){
+    res.render("auth/login",{
+      error: 'User does not exit'
+    });
+    return;
+  }
+
+  if(password !== user.pass){
+    res.render("auth/login",{
+      error: 'Wrong password'
+    });
+    return;
+  }
+  
+  res.redirect('/index');
+};
 
 module.exports.register = (req, res, next) => {
-    res.render('auth/register');
-}
+  db.get("user").push(req.body).write();
+  res.render("auth/register");
+};
--- SaiGonFood/routes/users.js
+++ SaiGonFood/routes/users.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const userController = require('../controllers/User.Controller');
 
 /* GET users listing. */
-router.get('/', userController.login);
 router.get('/login', userController.login);
+router.post('/login', userController.login);
+
 router.get('/register', userController.register);
+router.post('/register', userController.register);
 
 module.exports = router;
