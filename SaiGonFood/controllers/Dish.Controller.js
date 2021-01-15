19ea29b686cb53b0bbeef94919b98d59e18e3e6c
annalla <danghongxuyen2000@gmail.com>
2021-01-15 14:11:17 +0700

    fix bug rating

--- SaiGonFood/controllers/Dish.Controller.js
+++ SaiGonFood/controllers/Dish.Controller.js
@@ -9,23 +9,27 @@ db.defaults({ comment: [] }).write();
 
 module.exports.dish = (req, res, next) => {
   const comment = db.get('comment').value();
+  const rating = db.get('rate').value();
   res.render("single-food", {
-    rate: req.session.rating,
-    num_rate: req.session.num_rating,
+    rate: rating[0].rate,
+    num_rate:rating[0].num_rate,
     comment
   });
 };
 module.exports.rating = (req, res, next) => {
-  if (req.session.isAuth === false) {
-    res.render("auth/login", {
-      error: 'Cần login để đánh giá'
-    });
-    return;
-  }
+  // if (req.session.isAuth === false) {
+  //   res.render("auth/login", {
+  //     error: 'Cần login để đánh giá'
+  //   });
+  //   return;
+  // }
+  const rating = db.get('rate').value();
   var star1 = (+req.body.star) * 1.0;
-  req.session.num_rating += 1;
-  const avgRate = (req.session.rating * (req.session.num_rating - 1) + star1) / (req.session.num_rating);
-  req.session.rating = Math.round(avgRate * 100) / 100;
+  rating[0].num_rate += 1;
+  const avgRate = (rating[0].rate * (rating[0].num_rate - 1) + star1) / (rating[0].num_rate);
+  rating[0].rate = Math.round(avgRate * 100) / 100;
+  db.get('rate').find({ id: "rate" }).assign({rate: rating[0].rate }).value();
+  db.get('rate').find({ id: "rate" }).assign({num_rate: rating[0].num_rate }).value();
   res.redirect('/single-food');
 };
 
